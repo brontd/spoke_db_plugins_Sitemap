9f268252fa739e103a17cc3988d11741c2a56003
bront <bront.davis@uky.edu>
2024-08-22 15:19:22 -0400

    Update show.php
    Show.php has been edited to enable Ark urls... per Eric Weig

--- views/public/sitemap/show.php
+++ views/public/sitemap/show.php
@@ -13,16 +13,22 @@ $nav->loadAsOption(Omeka_Navigation::PUBLIC_NAVIGATION_MAIN_OPTION_NAME);
 $nav->addPagesFromFilter(Omeka_Navigation::PUBLIC_NAVIGATION_MAIN_FILTER_NAME);
 
 $collections = get_db()->getTable('Collection')->findAll();
+
+// changes made in order to utilize ARK URLs, Eric C. Weig 2019-05-01
+
 foreach($collections as $collection) {
+    $ark = metadata($collection,array('Dublin Core','Identifier'));
     $page = new Omeka_Navigation_Page_Mvc(array(
-        'label'      => metadata($collection,array('Dublin Core','Title')),
-        'route'      => 'id',
-        'action'     => 'show',
-        'controller' => 'collections',
-        'params'     => array('id' => $collection->id)
+//        'label'      => metadata($collection,array('Dublin Core','Title')),
+//        'route'      => 'id',
+//        'action'     => 'show',
+//        'controller' => 'collections',
+        'controller' => $ark,
+        'params'     => array('controller' => $collection->controller)
     ));
-    
+
     $nav->addPage($page);
+
 }
 
 if(plugin_is_active('ExhibitBuilder')){
@@ -38,5 +44,11 @@ if(plugin_is_active('ExhibitBuilder')){
     }
 }
 
-echo $this->navigation()->sitemap($nav);
+// changes made in order to utilize ARK URLs, Eric C. Weig 2019-05-01
+
+$nav = $this->navigation()->sitemap($nav);
+$nav = str_replace('%2F','/',$nav);
+$nav = str_replace('%3A',':',$nav);
+echo $nav;
+//echo $this->navigation()->sitemap($nav);
 ?>
